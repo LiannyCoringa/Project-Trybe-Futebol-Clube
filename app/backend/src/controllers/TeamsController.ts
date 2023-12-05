f7e715c0d978f45691016324f4bb5dbc1404c25e
Lianny Coringa <lianny_costa@hotmail.com>
2023-12-05 16:45:12 -0300

    Retirando o retorno do controller

--- app/backend/src/controllers/TeamsController.ts
+++ app/backend/src/controllers/TeamsController.ts
@@ -5,7 +5,7 @@ import mapStatusHTTP from '../utils/mapStatusHTTP';
 export default class TeamsController {
   constructor(private teamsService: TeamsService = new TeamsService()) { }
 
-  public async getAllTeams(req: Request, res: Response): Promise<Response> {
+  public async getAllTeams(req: Request, res: Response) {
     const { status, data } = await this.teamsService.getAllTeams();
     return res.status(mapStatusHTTP(status)).json(data);
   }
