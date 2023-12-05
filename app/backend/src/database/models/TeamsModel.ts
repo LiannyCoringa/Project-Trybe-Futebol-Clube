6e600c866e7d3a404f096643133b30682945f324
Lianny Coringa <lianny_costa@hotmail.com>
2023-12-05 11:48:12 -0300

    Primeiro requisito

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,33 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeams extends Model<InferAttributes<SequelizeTeams>,
+InferCreationAttributes<SequelizeTeams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  tableName: 'teams',
+});
+
+export default SequelizeTeams;
