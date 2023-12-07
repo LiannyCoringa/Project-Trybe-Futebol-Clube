fe938059bf129e85a122575b38b7c4b18033f1f5
Lianny Coringa <lianny_costa@hotmail.com>
2023-12-07 10:50:42 -0300

    Requisito 16 - filtros do inProgress

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express';
 import MatchesService from '../services/MatchesService';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
+import { IMatches } from '../Interfaces/matches/IMatches';
 
 export default class MatchesController {
   constructor(private matchesService: MatchesService = new MatchesService()) { }
 
-  public async getAllMatches(req: Request, res: Response) {
+  public async getAllMatches(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
     const { status, data } = await this.matchesService.getAllMatches();
+    const dataStringified = JSON.stringify(data);
+    if (inProgress) {
+      if (inProgress === 'true') {
+        const filteredData = JSON.parse(dataStringified)
+          .filter((match: IMatches) => match.inProgress);
+        return res.status(mapStatusHTTP(status)).json(filteredData);
+      }
+      const filteredData = JSON.parse(dataStringified)
+        .filter((match: IMatches) => match.inProgress === false);
+      return res.status(mapStatusHTTP(status)).json(filteredData);
+    }
     return res.status(mapStatusHTTP(status)).json(data);
   }
 }
